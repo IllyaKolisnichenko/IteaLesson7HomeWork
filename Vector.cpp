40dbfce0bc527aa54668c5d3dea28f329762ffff
IllyaKolisnichenko <kolisnichenko.ilya@yandex.ru>
2022-06-18 06:48:08 +0300

    Home work
    fixup! Home work

---
+++ Vector.cpp
@@ -0,0 +1,73 @@
+#include "Vector.h"
+
+Vector::Vector( int size )
+{
+}
+
+Vector::~Vector()
+{
+    delete[] vectorOfData_;
+}
+
+int Vector::max()
+{
+	int max;
+
+    return max;
+}
+
+int Vector::min()
+{
+	int min;
+
+    return min;
+}
+
+void Vector::sort()
+{
+	
+}
+
+void Vector::swap( int firstElement, int secondElement )
+{
+	
+}
+
+void Vector::resize( int size )
+{
+	
+}
+
+int Vector::front()
+{
+	return{};
+}
+
+int Vector::back()
+{
+    return{};
+}
+
+void Vector::printVector()
+{
+	
+}
+
+void Vector::pushBack( int value )
+{
+	
+}
+void Vector::append( int value, int position )
+{
+	
+}
+
+int Vector::size()
+{
+
+}
+
+void Vector::clear()
+{
+
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Vector.h"
+
+void testVector(){
+    Vector vector{10};
+    // test your vector
+}
+
+int main()
+{
+    testVector();
+}
