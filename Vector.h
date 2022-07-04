024b0352158ef372a0dd2638dba07e271fd9f6b3
ZaloginYuriy <yuriyzalogin31031981@gmail.com>
2022-07-04 15:53:04 +0300

    CUB5

--- Vector.h
+++ Vector.h
@@ -1,23 +1,27 @@
+
+
 #pragma once
 
-class Vector{
+class Vector {
 public:
-    Vector( int size );
-	~Vector();
+    Vector( int size);
+    ~Vector();
     int max();
     int min();
     void sort();
     int size();
     void clear();
-    void swap( int firstElement, int secondElement );
-    void resize( int size );
+    void swap(int firstElement, int secondElement);
+    void resize(int size);
     int front();
     int back();
+    void printVector1();
     void printVector();
-    void pushBack( int value );
-    void append( int value, int position );
+    void pushBack(int value);
+    void append(int value, int position);
 
 private:
-    int* vectorOfData_;
     int size_{};
+    int* vectorOfData_;
+   int position1;
 };
